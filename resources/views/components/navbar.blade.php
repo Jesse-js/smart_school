757a8a27a81750bede2df13ae5f8b2ab054fd3f5
Jesse-js <jesse-jjs@hotmail.com>
2024-07-15 23:07:24 -0300

    feat: adds navbar

---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,25 @@
+<nav class="uk-navbar-container" uk-navbar>
+    <div class="uk-navbar-left ml-2">
+        <ul class="uk-navbar-nav">
+            <li class="uk-active"><a href="#">Active</a></li>
+            <li>
+                <a href="#">Parent</a>
+                <div class="uk-navbar-dropdown">
+                    <ul class="uk-nav uk-navbar-dropdown-nav">
+                        <li class="uk-active"><a href="#">Active</a></li>
+                        <li><a href="#">Item</a></li>
+                        <li><a href="#">Item</a></li>
+                    </ul>
+                </div>
+            </li>
+            <li><a href="#">Item</a></li>
+        </ul>
+    </div>
+    <div class="uk-navbar-right mr-2">
+        <ul class="uk-navbar-nav">
+            <li class="uk-active">
+                <x-theme-toggle />
+            </li>
+        </ul>
+    </div>
+</nav>
