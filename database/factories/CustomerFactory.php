beea2ec439b9200511275f36b44c77660727c954
Jesse-js <jesse-jjs@hotmail.com>
2024-07-17 00:06:26 -0300

    feat: adds customer database layer

---
+++ database/factories/CustomerFactory.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Customer>
+ */
+class CustomerFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => fake()->name(),
+            'email' => fake()->unique()->safeEmail(),
+            'phone' => fake()->phoneNumber(),
+            'state' => fake()->state(),
+            'city' => fake()->city(),
+            'street' => fake()->streetName(),
+            'number' => fake()->buildingNumber(),
+            'zip' => fake()->postcode(),
+            'birthdate' => fake()->date(),
+            'cpf' => null,
+        ];
+    }
+}
